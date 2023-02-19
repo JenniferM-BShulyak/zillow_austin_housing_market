df77aa913b4830a2adc5df473bcfabb46fd50b2b
JenniferM-BShulyak <111457464+JenniferM-BShulyak@users.noreply.github.com>
2023-02-19 17:19:56 -0600

    Fixed toggle issues

--- templates/lineGraphsDraft.js
+++ templates/lineGraphsDraft.js
@@ -13,22 +13,22 @@ d3.selectAll("#toggleOptions").on("change", updateLineGraph);
 // Function to change plot with event is triggered
 function updateLineGraph() {
     // Select the dropdown menu
-    let toggle = d3.select("#toggleOptions")
+    let toggle = d3.select("#toggleOptions");
     
     // Assign the value of the event to a variable
-    let toggleChoice = toggle.property("value")
+    let toggleChoice = toggle.property("value");
 
     // Grab what the x and y values will now be
     // How will data be pulled from SQL? 
     let x = [];
     let y = [];
 
-    if (toggleChoice === "Zestimate") {
+    if (toggleChoice === "zestimates") {
         x = [1,2,3,4,5];
         y = [2,4,8,16,32];
     }
 
-    else if (toggleChoice === "Percent Change") {
+    else if (toggleChoice === "percentage") {
         x = [1,2,3,4,5];
         y = [3,9,27,81,243];
     }
