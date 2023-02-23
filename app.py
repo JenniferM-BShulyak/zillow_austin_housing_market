abcbbb4dbc0aff68a74ed937cebd50ae04a983fb
Jackson Blanton <Jackson@Jacksons-Air.lan>
2023-02-22 19:20:37 -0600

    updated app.py

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
-#Importing Functions
-from flask import Flask, render_template
-app = Flask(__name__)
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
-#root
-@app.route('/')
-def html():
-    return render_template('index.html')
+engine = create_engine('postgresql://postgres:Jb631834@localhost:5432/Austin_Housing')
 
-#Grabbing data from SQL
-#@app.route('/data')
-#def SQL():
-    #return 
+Base = automap_base()
 
+Base.prepare(autoload_with=engine, reflect = True)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug = True)
+Base.classes.keys()
+
+austin_metro = Base.classes.austin_metro
+austin_metro_percentages = Base.classes.austin_metro_percentages
+austin_zestimates = Base.classes.austin_zestimates
+austin_zestimates_percentages = Base.classes.austin_zestimates_percentages
+
+session = Session(engine)
+
+austin_metro.__dict__
+austin_metro_percentages.__dict__
+austin_zestimates.__dict__
+austin_zestimates_percentages.__dict__
+
+austin_metro_first = session.query(austin_metro).first()
+austin_metro_first.__dict__
+
+austin_metro_percentages_first = session.query(austin_metro_percentages).first()
+austin_metro_percentages_first.__dict__
+
+austin_zestimates_first = session.query(austin_zestimates).first()
+austin_zestimates_first.__dict__
+
+austin_zestimates_percentages_first = session.query(austin_zestimates_percentages).first()
+austin_zestimates_percentages_first.__dict__
+
+session.close()
