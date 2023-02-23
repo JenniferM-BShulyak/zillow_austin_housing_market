efd0b624cf4f64135c6f76cf6e6c92a6e44181ba
JenniferM-BShulyak <111457464+JenniferM-BShulyak@users.noreply.github.com>
2023-02-22 21:03:38 -0600

    got Raghu method for jsonifying sqlalchemy queries

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
+import pandas as pd
 
 # Create connection
 engine = create_engine('postgresql://postgres:Jb631834@localhost:5432/Austin_Housing')
@@ -12,13 +13,13 @@ engine = create_engine('postgresql://postgres:Jb631834@localhost:5432/Austin_Hou
 Base = automap_base()
 Base.prepare(autoload_with=engine, reflect = True)
 Base.classes.keys()
-session = Session(engine)
+
 
 # Tables
-austin_metro = Base.classes.austin_metro
-austin_metro_percentages = Base.classes.austin_metro_percentages
-austin_zestimates = Base.classes.austin_zestimates
-austin_zestimates_percentages = Base.classes.austin_zestimates_percentages
+Austin_metro = Base.classes.austin_metro
+Austin_metro_percentages = Base.classes.austin_metro_percentages
+Austin_zestimates = Base.classes.austin_zestimates
+Austin_zestimates_percentages = Base.classes.austin_zestimates_percentages
 
 # Create app
 app = Flask(__name__)
@@ -28,24 +29,28 @@ app = Flask(__name__)
 def page_name():
     return jsonify() # add routes
 
-austin_metro.__dict__
-austin_metro_percentages.__dict__
-austin_zestimates.__dict__
-austin_zestimates_percentages.__dict__
-
-austin_metro_first = session.query(austin_metro).first()
-austin_metro_first.__dict__
-
-austin_metro_percentages_first = session.query(austin_metro_percentages).first()
-austin_metro_percentages_first.__dict__
-
-austin_zestimates_first = session.query(austin_zestimates).first()
-austin_zestimates_first.__dict__
-
-austin_zestimates_percentages_first = session.query(austin_zestimates_percentages).first()
-austin_zestimates_percentages_first.__dict__
-
-session.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route('/vis_map_data')
+def map_data():
+    session = Session(engine)
+
+    austin_metro = session.query(Austin_metro)
+    austin_metro_data = [{k:v for k, v in row.__dict__.items() if k != '_sa_instance_state'} for row in austin_metro] 
+    austin_metro_json = pd.DataFrame(austin_metro_data).to_json
+
+    austin_metro_percentages = session.query(Austin_metro_percentages)
+
+    austin_zestimates = session.query(Austin_zestimates)
+
+    austin_zestimates_percentages_first = session.query(Austin_zestimates_percentages)
+    
+
+    session.close()
+
+   
+
+    return 
+    
