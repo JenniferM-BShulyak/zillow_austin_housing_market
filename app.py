c7044af55b4992c2000f47f553a517b9c749975c
jrm33 <jrmoreno03@gmail.com>
2023-02-22 19:35:27 -0600

    updated app.py, cleaned, added routes

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
+# Import functions
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify
 
+# Create connection
 engine = create_engine('postgresql://postgres:Jb631834@localhost:5432/Austin_Housing')
 
+# Reflect tables
 Base = automap_base()
-
 Base.prepare(autoload_with=engine, reflect = True)
-
 Base.classes.keys()
+session = Session(engine)
 
+# Tables
 austin_metro = Base.classes.austin_metro
 austin_metro_percentages = Base.classes.austin_metro_percentages
 austin_zestimates = Base.classes.austin_zestimates
 austin_zestimates_percentages = Base.classes.austin_zestimates_percentages
 
-session = Session(engine)
+# Create app
+app = Flask(__name__)
+
+# Create app routes
+@app.route('/')
+def page_name():
+    return jsonify() # add routes
 
 austin_metro.__dict__
 austin_metro_percentages.__dict__
@@ -36,3 +46,6 @@ austin_zestimates_percentages_first = session.query(austin_zestimates_percentage
 austin_zestimates_percentages_first.__dict__
 
 session.close()
+
+if __name__ == "__main__":
+    app.run(debug=True)
