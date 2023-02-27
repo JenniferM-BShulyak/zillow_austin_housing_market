e1c88935868b58f5f1985974460d8c795f5ca4ed
Jackson Blanton <Jackson@Jacksons-MacBook-Air.local>
2023-02-27 14:15:50 -0600

    updated index.html to corrected links for Flask app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, jsonify, render_template
 import pandas as pd
 
 # Create connection
-engine = create_engine('postgresql://postgres:2066325373@localhost:5432/zillow')
+engine = create_engine('postgresql://postgres:Jb631834@localhost:5432/Austin_Housing')
 
 # Reflect tables
 Base = automap_base()
