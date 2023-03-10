05cd83d1e1253921042fd2ceb1bcff36efe8c9ae
Omar A. Solis <oasj95@gmail.com>
2023-03-10 12:11:48 -0600

    leaflet update

--- static/map.js
+++ static/map.js
@@ -1,46 +1,37 @@
-// Access Token
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2JubG9tYXIiLCJhIjoiY2xlaHN1cGIwMGp3eDNzbnR6OWpscXl2MiJ9.mnkH-jHkJkD7XU6HoY1v_g';
+// Global Variables
+var myMap;
+var legend;
+var infoBox;
 
-// Adding Map
-const map = new mapboxgl.Map({
-// container ID
-container: 'map',
- // style URL
-style: 'mapbox://styles/gbnlomar/clem19jqn003f01mmydku5v7k',
-// starting position [lng, lat]
-center: [-97.733330, 30.266666],
-// starting zoom
-zoom: 10 
-});
 
-map.on('load', function(){
-    map.addLayer({
-        id: 'rent',
-        type: "fill",
-        source: {
-            type: 'geojson',
-            data: '..//geojson_data/geo_jsonmap/rent_geoclean.geojson'
-        },
-        'paint': {
-            'fill-color': [
-                'interpolate',
-                ['linear'],
-                ['get', '2022'],
-                1000,
-                '#008000',
-                2500,
-                '#7F3121'
-            ],
-            'fill-opacity': 0.75
-        }
-    });
+// Defom function to Create Map
+function creatMap(rentals){
 
-    // Set filter to first month of the year
-    // 0 = January
-    filterBy(0);
+    // Creating Map
+    myMap = L.map('map').setView([30.266666, -97.733330,], 10);
+    
+    // Map tiles
+    var baseMap = L.tileLayerL.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-    document.getElementById('slider').addEventListener('input', (e) => {
-        const month = parseInt(e.target.value, 8);
-        filterBy(month);
-    });
-})
+    // Calling function to create buttons 
+    createButtons();
+
+    //infoBox = L.control({position: 'bottomleft'});
+    // infoBox.onAdd = function({
+        //var div2 = L.DomUtil.create('div', 'info');
+        //div2.innerHTML += '<h2>Song Attribute Popularity Ranking</h2>\
+        //<h1><div id="this_country">Country Details</div></h1>\
+        //<h4><div id="attrs">Click a country for more info...</div></h4>';
+        //return div2;
+    //})
+    // Add info box to map
+    //infoBox.addTo(myMap);
+};
+
+
+
+// Adding Geojson
+
